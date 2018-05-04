2de4ffe56dabde529b0caa6d602f38f7d32b098a
Adeline Johnson <groblewski.adeline@gmail.com>
2018-05-03 23:58:52 -0400

    updated logic a bit

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,43 +1,29 @@
-var friends= require("../data/friends.js");
-
+var friends = require("../data/friends.js");
+var arr = [];
 
 module.exports = function (app) {
-    app.get("/api/friends", function (req, res){
-        res.json(friends); 
+    app.get("/api/friends", function (req, res) {
+        res.json(friends);
     })
 
-    app.post("/api/friends", function (req, res){
+    app.post("/api/friends", function (req, res) {
         var newfriend = req.body
         friends.push(newfriend);
         res.json(newfriend);
 
         var newfriendscore = req.body.scores;
-        var friendScores = [];
-        for (var i = 0; i < newfriendscore.length; i++) {
-            friendScores.push(parseInt(newfriendscore[i]));
-        }
-        console.log(friendScores);
         
-        var otherFriend = friends[0].scores; 
-        console.log(otherFriend);
-        var k;
-        var j; 
-        var newArray = [];
-        var reduced;
-
-        function getSum(total, num) {
-            return total + num;
-        }
-        
-        for (j = k = 0; j < friendScores.length && k < otherFriend.length; k++ && j++) {
-            console.log(otherFriend[k] - friendScores[j])
-            var newNumber = otherFriend[k] - friendScores[j];
-           newArray.push(newNumber)
-           reduced = newArray.reduce(getSum); 
-           
-        }
-        console.log(reduced);
-        
+        // loop through all the friends database. 
+        for (var i = 0; i < friends.length; i++) {
+            var scoreDifference = 0;
+ 
+            // loop through scores of each friend to calculate score difference
+            for (var h = 0; h < friends[i].scores[h]; h++) {
+                scoreDifference += Math.abs(parseInt(newfriendscore[h]) - parseInt(friends[i].scores[h]));
+                console.log(scoreDifference)
+            
+            }
+        }  
     });
 
 }
