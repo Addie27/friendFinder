9e83fb414cd4b5188295c31a8e1c0fb8695743f5
Adeline Johnson <groblewski.adeline@gmail.com>
2018-05-07 22:36:23 -0400

    updated logic

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,63 +4,63 @@ var friends = [
         photo :"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
         scores :[
             5,
-            1,
-            4,
-            4,
             5,
-            1,
-            2,
             5,
-            4,
-            1
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5
           ]
       },
       {
         name : "Homer Simpson",
         photo :"https://giphy.com/explore/homer-simpson",
         scores :[
-            1,
-            2,
-            5,
-            1,
-            5,
-            5,
-            5,
-            1,
             4,
-            3
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4
           ]
       },
       {
         name : "Chuck Norris",
         photo :"https://geeks.media/best-chuck-norris-movies-ever",
         scores :[
-            1,
-            5,
-            5,
-            5,
-            5,
-            5,
-            5,
-            5,
-            5,
-            1
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3
           ]
       },
       {
         name : "Fabio",
         photo :"https://boingboing.net/2016/03/17/fabio-is-american.html",
         scores :[
-            1,
-            4,
-            5,
-            5,
-            1,
-            1,
-            1,
-            1,
-            3,
-            5
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2
           ]
       }
       
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,23 +7,33 @@ module.exports = function (app) {
     })
 
     app.post("/api/friends", function (req, res) {
-        var newfriend = req.body
-        friends.push(newfriend);
-        res.json(newfriend);
-
         var newfriendscore = req.body.scores;
-        
+
+        var bestMatch = {
+            name: "",
+            photo: "",
+            friendDifference: 100
+        }
+
         // loop through all the friends database. 
         for (var i = 0; i < friends.length; i++) {
+            var friendScores = friends[i].scores
             var scoreDifference = 0;
- 
+
             // loop through scores of each friend to calculate score difference
-            for (var h = 0; h < friends[i].scores[h]; h++) {
-                scoreDifference += Math.abs(parseInt(newfriendscore[h]) - parseInt(friends[i].scores[h]));
-                console.log(scoreDifference)
-            
+            for (var h = 0; h < friendScores.length; h++) {
+                scoreDifference += Math.abs(parseInt(newfriendscore[h]) - parseInt(friendScores[h]));
+
             }
-        }  
+            if (scoreDifference <= bestMatch.friendDifference) {
+                bestMatch.name = friends[i].name;
+                bestMatch.photo = friends[i].photo;
+                bestMatch.friendDifference = scoreDifference;
+                
+            }     
+
+        }
+        console.log(bestMatch);
     });
 
 }
