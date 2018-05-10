fe81433aa45635a142e466c3e0ec1a90e8cdbf16
Adeline Johnson <groblewski.adeline@gmail.com>
2018-05-10 13:32:40 -0400

    clean up

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,66 +1,50 @@
 var friends = [
-    {
-        name : "Ahmed",
-        photo :"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        scores :[
-            5,
-            5,
-            5,
-            5,
-            5,
-            5,
-            5,
-            5,
-            5,
-            5
-          ]
-      },
       {
         name : "Homer Simpson",
         photo :"https://vignette.wikia.nocookie.net/youtubepoop/images/e/eb/Homer-simpson_WOOHOO.jpg/revision/latest?cb=20130120090101",
         scores :[
+            1,
+            3,
+            5,
+            1,
+            5,
             4,
-            4,
-            4,
-            4,
-            4,
-            4,
-            4,
-            4,
-            4,
-            4
+            3,
+            1,
+            5,
+            5
           ]
       },
       {
         name : "Chuck Norris",
         photo :"https://res.cloudinary.com/jerrick/image/upload/f_auto,fl_progressive,q_auto,c_fit,w_1100/wvanipvk1xocft2jsthp",
         scores :[
-            3,
-            3,
-            3,
-            3,
-            3,
-            3,
-            3,
-            3,
-            3,
-            3
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            1
           ]
       },
       {
         name : "Fabio",
         photo :"https://media.boingboing.net/wp-content/uploads/2016/03/fabio.jpg",
         scores :[
-            2,
-            2,
-            2,
-            2,
-            2,
-            2,
-            2,
-            2,
-            2,
-            2
+            1,
+            1,
+            5,
+            5,
+            1,
+            1,
+            1,
+            1,
+            5,
+            5
           ]
       }
       
